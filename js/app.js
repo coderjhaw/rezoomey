c304c9148602f86c1d196b389ade8aa6503d466f
coderjhaw <joshuaehd.silvio.37@gmail.com>
2022-11-12 21:04:58 -0500

    added style to navbar

--- js/app.js
+++ js/app.js
@@ -8,3 +8,25 @@ responsiveMenuToggleButton.addEventListener('click', () => {
   navBarLinks.classList.toggle('open');
   responsiveMenuToggleButton.classList.toggle('open');
 });
+
+
+// jquery
+
+$(document).ready(function() {
+  var scroll_start = 0;
+  var startchange = $('header');
+  var offset = startchange.offset();
+
+
+  if (startchange.length) {
+    $(document).scroll(function () {
+        scroll_start = $(this).scrollTop();
+        if (scroll_start > offset.top) {
+            $("header").css('background-color', 'rgba(0,0,0,.75)');
+        } else {
+            $("header").css('background-color', 'transparent');
+        }
+    });
+  }
+  
+});
